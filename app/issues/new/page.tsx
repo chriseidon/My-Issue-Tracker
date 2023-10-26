d3ba7b00a6cde23ad3310555d7df084d616ce005
chriseidon <chrisnanakwasi2005@gmail.com>
2023-10-25 23:14:37 -0300

    implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -8,16 +8,20 @@ import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
+import { Text } from '@radix-ui/themes';
 
-interface IssueForm {
-  title: string,
-  description: string
-}
+
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const newIssuePage = () => {
 
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema)
+  });
 
   const [error,  SetError] = useState('')
 
@@ -41,11 +45,13 @@ const newIssuePage = () => {
       <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
+        {errors.title && <Text color='red' as='p'>{ errors.title.message}</Text>}
         <Controller
           name='description'
          control={control}
           render={({ field }) => <SimpleMDE placeholder="Description" { ...field } /> }
         />
+        {errors.description && <Text color='red' as='p'>{ errors.description.message}</Text>}
             <Button className='cursor-pointer'>Submit New Issue</Button>
       </form>
     </div>
