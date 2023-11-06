cd2c80e8c3f146b03a5c53b4715117380e2b247c
chriseidon <chrisnanakwasi2005@gmail.com>
2023-11-06 04:11:20 -0800

    Refactor: Apply SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,48 +1,32 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBagde';
-import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import React from 'react'
-import Reactmarkdown from 'react-markdown';
-import delay from 'delay';
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link';
+import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import delay from "delay";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
-  params: { id: string }
+  params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  await delay(5000)
+  await delay(5000);
   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
+    where: { id: parseInt(params.id) },
   });
 
-  if (!issue)
-    notFound();
+  if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-     <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='prose' mt='4'>
-        <Reactmarkdown>{issue.description}</Reactmarkdown>
-      </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>
-          Edit Issue  
-          </Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
