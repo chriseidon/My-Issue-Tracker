6fa3385cf5fcbcfcf75eab871a0ba238a7266f14
Nana Kwasi <nanakwasi@Nanas-MacBook-Pro.local>
2023-11-27 04:39:17 -0800

    Improve The Loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,23 +1,19 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { issueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
 import { Button, Callout, TextField } from "@radix-ui/themes";
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { issueSchema } from "@/app/validationSchemas";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
-import { Issue } from "@prisma/client";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
