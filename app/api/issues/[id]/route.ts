54fc9977c78967391e5b8ac06b935d576eed93f6
Nana Kwasi <nanakwasi@Nanas-MacBook-Pro.local>
2023-11-28 05:58:30 -0800

    Delete an issue handling errors

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,15 +30,36 @@ export async function PATCH(
   return NextResponse.json(updatedIssue);
 }
 
-export async function Delete(
+// export async function Delete(
+//   request: NextRequest,
+//   { params }: { params: { id: string } }
+// ) {
+//   const issue = await prisma.issue.findUnique({
+//     where: { id: params.id },
+//   });
+
+//   if (!issue) return NextResponse.json("Invalid Issue"), { status: 404 };
+
+//   await prisma.issue.delete({
+//     where: { id: issue.id },
+//   });
+
+//   return NextResponse.json({});
+// }
+
+export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
+
   const issue = await prisma.issue.findUnique({
     where: { id: params.id },
   });
 
-  if (!issue) return NextResponse.json("Invalid Issue"), { status: 404 };
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
   await prisma.issue.delete({
     where: { id: issue.id },
