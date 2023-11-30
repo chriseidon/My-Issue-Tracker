3bb5a5ce9edd3bacab0b4a097fc50738bdd03e79
Nana Kwasi <nanakwasi@Nanas-MacBook-Pro.local>
2023-11-29 20:35:03 -0800

    Remove Duplicate Skeleton

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -36,8 +36,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setSubmitting(true);
       if (issue)
         await axios.patch("/api/issues/" + issue.id, data),
-          router.push("/issues");
-      else await axios.post("/api/issues", data), router.push("/issues");
+          router.push("/issues/list");
+      else await axios.post("/api/issues", data), router.push("/issues/list");
       router.refresh();
     } catch (error) {
       setSubmitting(false);
